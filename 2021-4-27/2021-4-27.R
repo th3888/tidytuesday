fcf69076044d26dbb2a5c3deb4e04581e6c81ac3
th3888 <hotake05@gmail.com>
2021-05-09 22:47:10 +0900

    data wrangling

--- 2021-4-27/2021-4-27.R
+++ 2021-4-27/2021-4-27.R
@@ -1,4 +1,19 @@
 library(tidyverse)
+library(skimr)
 
 CEO <- read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-04-27/departures.csv")
 
+#summary
+skim(CEO)
+
+#from departure code to  classifications
+CEO <- CEO %>% 
+  filter(!is.na(departure_code),
+         departure_code %in% 1:7) %>% 
+  mutate(departure_code = case_when(departure_code == 1 ~ "Death",
+                                    departure_code == 2 ~ "Illness",
+                                    departure_code == 3 ~ "Performance dismissal",
+                                    departure_code == 4 ~ "Legal dismissal",
+                                    departure_code == 5 ~ "Retired",
+                                    departure_code == 6 ~ "New opportunity",
+                                    departure_code == 7 ~ "Other"))
