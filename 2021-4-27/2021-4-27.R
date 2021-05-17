407fd5592435106182bbd0a2ba6f95358f588f85
th3888 <hotake05@gmail.com>
2021-05-17 23:37:02 +0900

    commit

--- 2021-4-27/2021-4-27.R
+++ 2021-4-27/2021-4-27.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(skimr)
+library(tidymodels)
 
 CEO <- read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-04-27/departures.csv")
 
