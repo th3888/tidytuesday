7813907b066552ba3e0a7414c1558ca8d75a2d5f
th3888 <hotake05@gmail.com>
2022-05-31 23:39:03 +0900

    commit

--- 2022-05-31/2022-05-31.R
+++ 2022-05-31/2022-05-31.R
@@ -5,7 +5,8 @@ reputation <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience
 
 poll %>% 
   group_by(industry, year) %>% 
-  summarise(avg_rq = mean(rq))
+  summarise(avg_rq = mean(rq), .groups = "drop") %>% 
+  filter(!is.na(avg_rq)) %>% 
   ggplot() + 
-  geom_line(aes(x = year, y = rq, color = company))
+  geom_line(aes(x = year, y = avg_rq, color = industry))
   
