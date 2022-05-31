a0f213f86f179aff19b0361ebb521377f8937d8d
th3888 <hotake05@gmail.com>
2022-05-31 21:29:49 +0900

    initial commit

---
+++ 2022-05-31/2022-05-31.R
@@ -0,0 +1,5 @@
+library(tidyverse)
+
+poll <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-05-31/poll.csv')
+reputation <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-05-31/reputation.csv')
+
