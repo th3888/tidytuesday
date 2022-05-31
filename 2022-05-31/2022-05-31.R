0870ea7c5debe50d1fb2bfb2b2c180850d9733d9
th3888 <hotake05@gmail.com>
2022-05-31 23:30:04 +0900

    commit

--- 2022-05-31/2022-05-31.R
+++ 2022-05-31/2022-05-31.R
@@ -3,3 +3,9 @@ library(tidyverse)
 poll <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-05-31/poll.csv')
 reputation <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-05-31/reputation.csv')
 
+poll %>% 
+  group_by(industry, year) %>% 
+  summarise(avg_rq = mean(rq))
+  ggplot() + 
+  geom_line(aes(x = year, y = rq, color = company))
+  
